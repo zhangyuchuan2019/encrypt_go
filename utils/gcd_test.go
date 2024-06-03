ad4738104e689743c54f717216b081abf131a687
zhangyuchuan2022 <zhangyuchuan2022@bytedance.com>
2024-06-03 19:00:27 +0800

    feat: rsa

---
+++ utils/gcd_test.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGCD(t *testing.T) {
+	a := int64(70)
+	b := int64(20)
+	x, y := GCD(a, b)
+	fmt.Printf("GCD(%d, %d) = %d*%d + %d*%d\n", a, b, x, a, y, b)
+}
+
+func TestGetGCD(t *testing.T) {
+	a := int64(70)
+	b := int64(20)
+	gcd := GetGCD(a, b)
+	fmt.Printf("GCD(%d, %d) = %d\n", a, b, gcd)
+	if gcd != 10 {
+		t.Errorf("GCD(%d, %d) != 10", a, b)
+	}
+}
